c8924a895372b099705a006b4bf36c58c7e9764c
tr1ggerbug <lequockhanhkt03@gmail.com>
2022-01-21 12:10:39 +0700

    add endpoint

--- src/common/errors/AppError.js
+++ src/common/errors/AppError.js
@@ -1,14 +1,14 @@
 class AppError extends Error {
-	constructor(statusCode, msg) {
-		super();
+    constructor(statusCode, msg) {
+        super()
 
-		if (Error.captureStackTrace) {
-			Error.captureStackTrace(this, AppError);
-		}
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, AppError)
+        }
 
-		this.message = msg;
-		this.statusCode = statusCode;
-	}
+        this.message = msg
+        this.statusCode = statusCode
+    }
 }
 
-module.exports = { AppError };
+module.exports = { AppError }
