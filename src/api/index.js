99e38f9eddc5571a69cbb11ff83f41ef2e33faeb
Hieu V <votruongtrunghieu@gmail.com>
2022-01-22 23:10:29 +0700

    tmp

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const purchase = require('./purchase');
 // const items = require('./items');
 const cart = require('./cart');
-const auth = require('./auth');
+//const auth = require('./auth');
 
 router.use('/purchase', purchase);
-// router.use('/items', items);
+router.use('/items', items);
 router.use('/auth', auth);
 router.use('/cart', cart);
 
