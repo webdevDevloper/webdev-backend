ca3ef94a8d9bf79120754537d3a7a19693bc5b12
Hieu V <votruongtrunghieu@gmail.com>
2022-01-22 21:16:22 +0700

    Fix APIS

--- src/api/items/items.controller.js
+++ src/api/items/items.controller.js
@@ -3,25 +3,25 @@ const itemsService = require('./items.service');
 const { AppError } = require('../../common/errors/AppError');
 
 module.exports = {
-    getItemsByName: async (req, res) => {
+    getItemsByName: async (req, res, next) => {
         try {
             res.send(await itemsService.getItemsByName(req.query.name));
         } catch (err) {
             next(err);
         }
     },
-    getAllItems: async (req, res) => {
+    getAllItems: async (req, res, next) => {
         try {
             res.send(await itemsService.getAllItems(res));
         } catch (err) {
             next(err);
         }
     },
-    getItemDetail: async (req, res) => {
+    getItemDetail: async (req, res, next) => {
         try {
             res.send(await itemsService.getItemDetail(req.params.id));
         } catch (err) {
-            return next(err);
+            next(err);
         }
     },
     uploadItem: async (req, res, next) => {
@@ -43,7 +43,6 @@ module.exports = {
             let imageUrl = await itemsService.uploadThumbnail(req.file.path, req.file.filename);
 
             // Save data to mongodb
-
             const response = await itemsService.uploadItem(
                 '61d634706a98a61edd42bf45',
                 title,
--- src/api/items/items.service.js
+++ src/api/items/items.service.js
@@ -3,6 +3,7 @@ const { AppError } = require('../../common/errors/AppError');
 const cloudinary = require('cloudinary');
 const Product = require('../../models/productModel').Product;
 const { mongoose } = require('mongoose');
+const successRes = require('../../common/utils/Response').successRes;
 
 module.exports = {
     uploadThumbnail: async (path, name) => {
@@ -16,11 +17,6 @@ module.exports = {
         return ret;
     },
     uploadItem: async (userID, title, description, price, amount, imageUrl) => {
-        const successRes = {
-            statusCode: 200,
-            message: 'ok',
-        };
-
         const product = new Product({
             userID,
             title,
@@ -37,51 +33,42 @@ module.exports = {
                 throw new AppError(500, "Can't upload item");
             });
 
+        delete successRes.data;
+
         return successRes;
     },
     getAllItems: async () => {
-        let response = {};
-
         await Product.find({})
-            .then((res) => (response = res))
+            .then((res) => (successRes.data = res))
             .catch((err) => {
-                if (err) {
-                    throw new AppError(500, "Can't get items");
-                }
+                throw new AppError(500, "Can't get items");
             });
 
-        return response;
+        return successRes;
     },
     getItemsByName: async (name) => {
-        let response = {};
-
         await Product.find({
             title: {
                 $regex: name,
             },
         })
-            .then((res) => (response = res))
+            .then((res) => (successRes.data = res))
             .catch((err) => {
-                if (err) {
-                    throw new AppError(500, "Can't get items");
-                }
+                throw new AppError(500, "Can't get items");
             });
-
-        return response;
+        return successRes;
     },
     getItemDetail: async (id) => {
-        let response = {};
-
         await Product.find({
             _id: id,
         })
-            .then((res) => (response = res))
+            .then((res) => {
+                successRes.data = res;
+                // 404
+            })
             .catch((err) => {
-                if (err) {
-                    throw new AppError(500, "Can't get item detail");
-                }
+                throw new AppError(500, "Can't get item detail");
             });
-
-        return response;
+        return successRes;
     },
 };
