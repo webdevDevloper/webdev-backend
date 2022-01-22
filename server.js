91a502fe31ac7afdfacd0a3d18a8f88c7a37d493
chaunhutlong <longuit2002@gmail.com>
2022-01-22 22:24:28 +0700

    merge

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ mongoose
     })
     .then(() => console.log('DB connection successful!'));
 
-const api = require('./src/api');
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
