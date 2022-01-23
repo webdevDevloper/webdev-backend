8cf5da94a8a6fb16a25491fabeb6493606c7fea0
tr1ggerbug <lequockhanhkt03@gmail.com>
2022-01-23 21:45:47 +0700

    add catch error in validate token

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -4,45 +4,46 @@ const { promisify } = require('util');
 const User = require('../models/userModel');
 
 exports.validate = async (req, res, next) => {
-    // 1) Getting token and check of it's there
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
-    } else if (req.cookies.jwt) {
-        token = req.cookies.jwt;
+    try {
+        // 1) Getting token and check of it's there
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        } else if (req.cookies.jwt) {
+            token = req.cookies.jwt;
+        }
+
+        if (!token) {
+            return res.status(401).json({
+                statusCode: 401,
+                message: 'You are not logged in! Please log in to get access.',
+            });
+        }
+
+        // 2) Verification token
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+        // 3) Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+            return res.status(401).json({
+                statusCode: 401,
+                message: 'The user belonging to this token does no longer exist.',
+            });
+        }
+
+        // 4) Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+            return res.status(401).json({
+                statusCode: 401,
+                message: 'User recently changed password! Please log in again.',
+            });
+        }
+
+        // GRANT ACCESS TO PROTECTED ROUTE
+        req.user = currentUser;
+        next();
+    } catch (error) {
+        next(new AppError(403, error.message));
     }
-
-    if (!token) {
-        res.status(401).json({
-            statusCode: 401,
-            message: 'You are not logged in! Please log in to get access.',
-        });
-        return;
-    }
-
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) {
-        res.status(401).json({
-            statusCode: 401,
-            message: 'The user belonging to this token does no longer exist.',
-        });
-        return;
-    }
-
-    // 4) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
-        res.status(401).json({
-            statusCode: 401,
-            message: 'User recently changed password! Please log in again.',
-        });
-        return;
-    }
-
-    // GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser;
-    next();
 };
