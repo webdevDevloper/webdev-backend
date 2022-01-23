85a8c781f01085a0bf3569430d58263a39e2297f
Khánh <55197200+tr1ggerbug@users.noreply.github.com>
2022-01-23 22:05:37 +0700

    Update userPermission.js

--- src/middleware/userPermission.js
+++ src/middleware/userPermission.js
@@ -3,7 +3,7 @@ const { AppError } = require('../common/errors/AppError');
 exports.userPermission = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            res.status(401).json({
+            return res.status(401).json({
                 statusCode: 401,
                 message: '401_UNAUTHORIZED',
             });
